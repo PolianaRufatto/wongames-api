743ac586bc5dd4a5caa2941cb8962208e85079fa
Poliana Rufatto <polianarufatto@gmail.com>
2021-01-13 06:40:40 -0300

    Create publisher/developer via service

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -26,8 +27,21 @@ module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
-    const { data: { products } } = await axios.get(gogApiUrl)
+    const { data: { products } } = await axios.get(gogApiUrl);
 
-    console.log(await getGameInfo(products[0].slug));
+    console.log(products[0]);
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase(),
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase(),
+    });
+
+
+    // console.log(await getGameInfo(products[0].slug));
   }
 };
